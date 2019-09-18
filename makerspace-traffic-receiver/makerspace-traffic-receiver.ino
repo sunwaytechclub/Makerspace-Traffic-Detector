fc38cbe2bbcbd62d3f4ec4c266e62efae0fed2cb
lewjiayi <jiaareaper@hotmail.com>
2019-09-18 11:24:39 +0800

    updated receiver

--- makerspace-traffic-receiver/makerspace-traffic-receiver.ino
+++ makerspace-traffic-receiver/makerspace-traffic-receiver.ino
@@ -4,16 +4,16 @@
 
 static const Country country = COUNTRY_SG;  //  Set this to your country to configure the Sigfox transmission frequencies.
 static const bool useEmulator = false;      //  Set to true if using UnaBiz Emulator.
-static const String device = "2C3068";       //  Set this to your device name if you're using UnaBiz Emulator.
+static const String device = "2C3171";       //  Set this to your device name if you're using UnaBiz Emulator.
 static const bool echo = true;              //  Set to true if the Sigfox library should display the executed commands.
-//static String response;
 
 static UnaShieldV2S transceiver(country, useEmulator, device, echo);  //Create transceiver object
 
 const unsigned int transmit_message_interval = 10;                      //Sets the duration of the interval (in minutes) between successive message transmissions.
 unsigned long previous_timestamp_m = 0L;                                //Timestamp of the beginning of the previous iteration of void loop() in milliseconds.
 unsigned long previous_iteration_duration = 0L;                          //Duration of execution of previous iteration of void loop() in milliseconds.
-long countdown = transmit_message_interval * 60L * 1000L;                //Countdown timer for message transmission.
+long countdown = transmit_message_interval * 60L * 500L;                //Countdown timer for message transmission.
+String msg;
 
 unsigned int counter = 0;
 unsigned long duration_low = 0L;
@@ -28,9 +28,6 @@ void setup()
   {
     stop(F("Unable to init Sigfox module, something is missing"));
   }
-//  transceiver.sendMessageAndGetResponse("0102030405060708090a0b0c", response);
-//  Serial.print(F("gvyvgjkbkhjkgvjghvy is "));
-//  Serial.println(response);
 }
 
 unsigned int count_when_blocking()
@@ -61,14 +58,18 @@ unsigned int count_when_blocking()
     {
       blocking_control = 0; 
       blocking_time_m += 95;
-//      Serial.println(blocking_time_m);
       if(blocking_time_m >= 2000)
       {
         count += 1;
         blocking_time_m = 0;
+        Serial.println("Counting block: " + (String) count);
       }
     }
-//    Serial.println(count);
+  }
+
+//if no one if blocking, means one person has passed by
+  if (count == 0){
+    count = 1;
   }
   
   return count;
@@ -84,27 +85,34 @@ void loop()
   duration_low = pulseIn(INPUT_PIN, LOW, TIMEOUT_LENGTH);
   if(duration_low == 0)
   {
-      //detected person, add one to counter
-      counter += 1;
-      //person blocking signal, set blocking flag
+      //detected person blocking signal, set blocking flag
       blocking_flag = true;
       counter += count_when_blocking();
+      Serial.println("Current count: " + (String)counter);
   }
 
-  if(countdown <= 0){
-    Serial.print("Sending counter = ");
-    Serial.println(counter);
-    transceiver.sendString(String(counter));
 
+  if(countdown <= 0){
+    Serial.println("Sending counter = " + (String)counter);
+    //backend can only support up to 3 digits
+    if (counter > 999){
+      msg = "999";
+    }
+    else if (counter < 10){
+      msg = "00" + (String) counter;
+    }
+    else if (counter < 100){
+      msg = "0" + (String) counter;
+    }
+    else {
+      msg = (String) counter;
+    }
+    transceiver.sendString(msg);
 
     //Reset after sending counter.
     counter = 0;
-    countdown = transmit_message_interval * 60L * 1000L;
+    countdown = transmit_message_interval * 60L * 500L;
     duration_low = 0L;
     blocking_flag =  false;
   }
-  
-  Serial.println(counter);
 }
-
-
